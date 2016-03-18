2ae328446bbc9345b78dc3b7062b1484d3413a04
sout0053 <maxwell.southwood@outlook.com>
2016-03-18 19:00:14 -0400

    Fixed indentation in html
    Got rid of empty space in javascript

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ var $ball = $('div');
 var $picker = $('form');
 var $input = $('#pick-color');
 
-
 $picker.on('change', function() {
   console.log('works');
   $ball.css('background-color', $input.val());
