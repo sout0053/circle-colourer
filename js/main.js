e38fc42b6c65c42cc0e15e843c197ec89ba9af80
sout0053 <maxwell.southwood@outlook.com>
2016-03-18 19:01:19 -0400

    Added expected space

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var $ball = $('div');
 var $picker = $('form');
 var $input = $('#pick-color');
 
-$picker.on('change', function() {
+$picker.on('change', function () {
   console.log('works');
   $ball.css('background-color', $input.val());
-  });
+});
