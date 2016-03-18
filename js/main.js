82d4064e915c3a1fedd4fb074900d82fbb27b396
sout0053 <maxwell.southwood@outlook.com>
2016-03-18 18:56:01 -0400

    Added javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var $html = $('html');
+var $ball = $('div');
+var $picker = $('form');
+var $input = $('#pick-color');
+
+
+$picker.on('change', function() {
+  console.log('works');
+  $ball.css('background-color', $input.val());
+  });
